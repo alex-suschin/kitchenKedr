9d689852f19511c4489e94a8a79c73c9a2487f04
alex-suschin <alex_suschin@mail.ru>
2020-04-27 16:37:27 +0300

    decors-cat-full

---
+++ src/blocks/modules/decors-items/decors-items.js
@@ -0,0 +1,11 @@
+import "../../../js/import/jquery.formstyler.min";
+import "../../../js/import/jquery.fancybox.min";
+$(function() {
+
+    setTimeout(function() {
+        $('input[type=checkbox]').styler();
+    }, 100)
+
+
+
+});
--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,4 @@ import "./import/modules";
 import "./import/slick.min";
 import "%modules%/slider-vn/slider-vn";
 import "%modules%/social-life/social-life";
+import "%modules%/decors-items/decors-items";

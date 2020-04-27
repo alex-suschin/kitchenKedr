605f79137f4dac4906628c7e8f9de83d552e8497
alex-suschin <alex_suschin@mail.ru>
2020-04-27 12:22:49 +0300

    about-page-full

---
+++ src/blocks/modules/social-life/social-life.js
@@ -0,0 +1,12 @@
+$(function() {
+    $('.social-life__slider').slick({
+        dots: false,
+        arrows: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        variableWidth: true,
+        swipeToSlide: true
+    });
+});
--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,4 @@
 import "./import/modules";
 import "./import/slick.min";
 import "%modules%/slider-vn/slider-vn";
+import "%modules%/social-life/social-life";

d1f3aaf6a2709ad79c8e8479d8058af6aafd3818
alex-suschin <alex_suschin@mail.ru>
2020-05-06 16:04:51 +0300

    popup1-cities

--- src/blocks/modules/cities/cities.js
+++ src/blocks/modules/cities/cities.js
@@ -5,4 +5,47 @@ $(function() {
     //     columnWidth: 245
     // });
 
+
+    $('.btn-popup-buy').click(function(e) {
+        $(this).addClass('active');
+
+        var popupCont = $('#popup-buy');
+
+
+        if (popupCont.hasClass('popup-active') == false) {
+            $('#popup-buy-wrap').addClass('popup-active');
+            $('#popup-buy').addClass('popup-active');
+            $('html').addClass('ov-h');
+
+
+            var firstClickCart = true;
+            $(document).bind('click.myEventCart', function(e) {
+                if (!firstClickCart && $(e.target).closest('#popup-buy').length == 0) {
+                    $('#popup-buy-wrap').removeClass('popup-active');
+                    $('#popup-buy').removeClass('popup-active');
+                    $('.btn-popup-buy').removeClass('active');
+                    $('html').removeClass('ov-h');
+                    $(document).unbind('click.myEventCart');
+                }
+
+                firstClickCart = false;
+            });
+        }
+
+        e.preventDefault();
+    });
+
+    $('.close-popup').click(function() {
+        $('#popup-buy-wrap').removeClass('popup-active');
+        $('#popup-buy').removeClass('popup-active');
+        $('.btn-popup-buy').removeClass('active');
+        $('html').removeClass('ov-h');
+        $(document).unbind('click.myEventCart');
+    });
+
+    // $('.btn-popup-buy').click(function() {
+    //     $('#popup-buy-wrap').addClass('popup-active');
+    //     $('#popup-buy-wrap').find('#popup-buy').addClass('popup-active');
+    // })
+
 });

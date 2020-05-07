dcd2b3ec8ec2124217a53e9fe0cbd9b3ae84366a
alex-suschin <alex_suschin@mail.ru>
2020-05-07 10:52:14 +0300

    full

--- src/blocks/modules/header/header.js
+++ src/blocks/modules/header/header.js
@@ -1,10 +1,39 @@
-var hamburger = $('#hamburger-icon');
-var menu = $('.top-menu');
-hamburger.click(function() {
-    hamburger.toggleClass('active');
-    menu.toggleClass('active');
-    $('html').toggleClass('ov-h');
-    return false;
+// var hamburger = $('#hamburger-icon');
+// var menu = $('.top-menu');
+// hamburger.click(function() {
+//     hamburger.toggleClass('active');
+//     menu.toggleClass('active');
+//     $('html').toggleClass('ov-h');
+//     return false;
+// });
+
+
+$('#hamburger-icon').click(function(i) {
+    $(this).addClass('active');
+
+    var menuMob = $('top-menu');
+
+
+    if (menuMob.hasClass('active') == false) {
+        $('#hamburger-icon').addClass('active');
+        $('.top-menu').addClass('active');
+        $('html').addClass('ov-h');
+
+
+        var firstClickMenu = true;
+        $(document).bind('click.myEventMenu', function(i) {
+            if (!firstClickMenu && $(i.target).closest('.top-menu').length == 0) {
+                $('#hamburger-icon').removeClass('active');
+                $('.top-menu').removeClass('active');
+                $('html').removeClass('ov-h');
+                $(document).unbind('click.myEventMenu');
+            }
+
+            firstClickMenu = false;
+        });
+    }
+
+    i.preventDefault();
 });
 
 
